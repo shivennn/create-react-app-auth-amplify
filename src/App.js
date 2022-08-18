20781badf0e84742cabc000e12016671708a9d01
shivennn <nshiven@amazon.com>
2022-08-18 15:22:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            This is test
           </a>
         </header>
       </div>
